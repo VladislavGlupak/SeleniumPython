5303c852b37535554eb63fc7c558aaf86d8e7df2
Vladislav Glupak <vglupak@gmail.com>
2019-12-23 22:28:40 -0800

    new

--- my_study.py
+++ my_study.py
@@ -6,7 +6,3 @@ driver.get('http://www.google.com')
 
 driver.find_element_by_css_selector(".gLFyf").send_keys("Selenium WebDriver")
 driver.find_element_by_css_selector(".FPdoLc > center:nth-child(1) > input:nth-child(1)").click()
-
-driver.find_element_by_css_selector()
-
-sddsvddfbsdfb
